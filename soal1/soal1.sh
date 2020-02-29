ccf1bbd9c0e80b79e4e2ed3827bdc3ca15d5fc79
ilhambayhaqi <57692117+ilhambayhaqi@users.noreply.github.com>
2020-02-29 20:51:44 +0700

    Update soal1.sh

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -19,6 +19,10 @@ sort -n -t '-' -k2 -o Region.txt Region.txt &&
 minReg="$(awk -F'|' 'NR==1{ print $1 }' Region.txt)"
 echo -n "$minReg" > Region.txt && minReg="$(cat Region.txt)"
 
+echo "Region :"
+echo "$(cat Region.txt)"
+echo $'\n'
+
 #bagian B
 awk -v var=$minReg '
 BEGIN{
@@ -39,6 +43,10 @@ sort -h -t '|' -k2 -o State.txt State.txt &&
 
 minState="$(awk -F'|' 'NR<=2{ print $1 }' State.txt)"
 echo "$minState" > State.txt && minState="$(cat State.txt)"
+
+echo "State :"
+echo "$(cat State.txt)"
+echo $'\n'
 
 #Bagian C Versi 1 (10 Product dari State bagian B dicampur)
 awk -v var="$minState" '
@@ -63,6 +71,10 @@ sort -h -t '|' -k2 -o ProductName.txt ProductName.txt &&
 
 minProduct="$(awk -F'|' 'NR<=10{ print $1 }' ProductName.txt)"
 echo "$minProduct" > ProductName.txt && minProduct="$(cat ProductName.txt)"
+
+echo "ProductName :"
+echo "$(cat ProductName.txt)"
+echo $'\n'
 
 #Bagian C Versi 2(10 Product dari masing masing State bagian B)
 awk -v var="$minState" '
@@ -95,4 +107,7 @@ while read -r i ; do
 	sort -h -t '|' -k2 -o "ProductName${i}.txt" "ProductName${i}.txt"
 	minProduct="$(awk -F'|' 'NR<=10{ print $1 }' "ProductName${i}.txt")"
 	echo "$minProduct" > "ProductName${i}.txt"
+	echo "ProductName ${i} :"
+	echo "$(cat ProductName${i}.txt)"
+	echo $'\n'
 done < State.txt
